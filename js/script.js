69b0744aab743dcb4971807222597458c758b6cd
Vitor <vitordelavedova@hotmail.com>
2020-09-01 21:03:20 -0300

    Correção js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var botoes = document.getElementsByName('button');
 
-botoes[0].addEventListener('click', bemVindo);
+botoes[0].addEventListener('click', bemVindo());
 
 function bemVindo(){
     window.alert('Seja Bem vindo');
